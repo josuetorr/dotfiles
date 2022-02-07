e59b83234534b48537cc2a90670ceb0ce16e035b
Josue Torres <josue.dtorres@gmail.com>
2022-02-07 14:39:51 -0500

    added emmet

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -65,6 +65,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- emmet
+  use 'mattn/emmet-vim'
+
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
