cf4f57eaae62f7bd7c746c2614f4fb3f8a87fdab
Josue Torres <josue.dtorres@gmail.com>
2022-02-22 18:02:10 -0500

    update

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,6 +1,7 @@
-local colo = "nord"
+local colo = "moonlight"
 
 vim.g.nord_contrast = true
+vim.g.material_style = "deep ocean"
 
 local status_ok, _ = pcall(vim.cmd, "colo " .. colo)
 if not status_ok then
