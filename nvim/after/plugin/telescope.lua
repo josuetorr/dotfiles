4332018b922f1d8397077bce47f6a8e6b7710a24
Josue Torres <josue.torres@Josue-Torres-Mac.local>
2023-08-18 10:18:39 -0400

    remapped telescope keymaps to [S]earch

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>f", builtin.find_files)
-vim.keymap.set("n", "<leader>g", builtin.git_files)
-vim.keymap.set("n", "<leader>w", builtin.grep_string)
-vim.keymap.set("n", "<leader>s", builtin.live_grep)
+vim.keymap.set("n", "<leader>sf", builtin.find_files)
+vim.keymap.set("n", "<leader>sg", builtin.git_files)
+vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+vim.keymap.set("n", "<leader>ss", builtin.live_grep)
