711b83ab535b3f904c59eb81731870768223e43a
Josué Torres <josue.torres@vooban.com>
2024-10-23 21:32:06 -0400

    added vim as global to luals

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -72,18 +72,18 @@ return {
         local opts = { buffer = bufnr, remap = false }
         wk.add({
           {
-            { "g",  group = "code" },
-            { "gd", vim.lsp.buf.definition,      desc = "Jump to definition" },
-            { "gD", vim.lsp.buf.declaration,     desc = "Jump to declaration" },
-            { "gi", vim.lsp.buf.implementation,  desc = "Jump to implementation" },
+            { "g", group = "code" },
+            { "gd", vim.lsp.buf.definition, desc = "Jump to definition" },
+            { "gD", vim.lsp.buf.declaration, desc = "Jump to declaration" },
+            { "gi", vim.lsp.buf.implementation, desc = "Jump to implementation" },
             { "gt", vim.lsp.buf.type_definition, desc = "Jump to type definition" },
-            { "gR", vim.lsp.buf.references,      desc = "Show references" },
-            { "gr", vim.lsp.buf.rename,          desc = "Rename symbol" },
-            { "gs", vim.lsp.buf.signature_help,  desc = "Display signature info" },
-            { "gx", vim.lsp.buf.code_action,     desc = "Code actions" },
-            { "go", vim.diagnostic.open_float,   desc = "Show diagnostic window" },
-            { "gj", vim.diagnostic.goto_next,    desc = "Goto next diagnostic" },
-            { "gk", vim.diagnostic.goto_prev,    desc = "Goto previous diagnostic" },
+            { "gR", vim.lsp.buf.references, desc = "Show references" },
+            { "gr", vim.lsp.buf.rename, desc = "Rename symbol" },
+            { "gs", vim.lsp.buf.signature_help, desc = "Display signature info" },
+            { "gx", vim.lsp.buf.code_action, desc = "Code actions" },
+            { "go", vim.diagnostic.open_float, desc = "Show diagnostic window" },
+            { "gj", vim.diagnostic.goto_next, desc = "Goto next diagnostic" },
+            { "gk", vim.diagnostic.goto_prev, desc = "Goto previous diagnostic" },
           },
           {
             "<leader>p",
@@ -107,7 +107,7 @@ return {
       lsp_zero.extend_lspconfig({
         capabilities = require("cmp_nvim_lsp").default_capabilities(),
         lsp_attach = lsp_attach,
-        float_border = "rounder",
+        float_border = "rounded",
         sign_text = true,
       })
 
@@ -116,6 +116,14 @@ return {
         ensure_installed = {},
         handlers = {
           lsp_zero.default_setup,
+          lua_ls = {
+            settings = {
+              diagnostics = {
+                enable = true,
+                globals = { "vim" },
+              },
+            },
+          },
           emmet_language_server = function()
             lsp_zero.configure("emmet_language_server", {
               filetypes = {
